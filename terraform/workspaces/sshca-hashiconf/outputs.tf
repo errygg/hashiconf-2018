76c08815829e575e543d660393fce9374df77187
Erik R. Rygg <errygg@gmail.com>
2018-10-11 21:07:19 -0600

    Update output refs

--- terraform/workspaces/sshca-hashiconf/outputs.tf
+++ terraform/workspaces/sshca-hashiconf/outputs.tf
@@ -1,4 +1,4 @@
 output "ca_client_public_ip" {
   description = "SSH CA Client Public IP Address"
-  value       = "${module.ssh_clients.ca_client_public_ip}"
+  value       = "${module.ssh_client_ca.ca_client_public_ip}"
 }
--- terraform/workspaces/sshotp-hashiconf/outputs.tf
+++ terraform/workspaces/sshotp-hashiconf/outputs.tf
@@ -1,4 +1,4 @@
 output "otp_client_public_ip" {
   description = "SSH OTP Client Public IP Address"
-  value       = "${module.ssh_clients.otp_client_public_ip}"
+  value       = "${module.ssh_client_otp.otp_client_public_ip}"
 }
