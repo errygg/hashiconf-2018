6394fe695bc424d0b4c54122340d190251f7754e
Erik R. Rygg <errygg@gmail.com>
2018-10-11 21:18:51 -0600

    Fix key_name data reference

--- terraform/workspaces/sshca-hashiconf/main.tf
+++ terraform/workspaces/sshca-hashiconf/main.tf
@@ -8,7 +8,7 @@ data "terraform_remote_state" "vault" {
 module "ssh_client_ca" {
   source = "../../modules/terraform-aws-ssh-client-ca"
   
-  key_name               = "${data.terraform_remote_state.vault.key_name}"
+  key_name               = "${data.terraform_remote_state.vault.ssh_key_name}"
   subnet_id              = "${data.terraform_remote_state.vault.subnet_public_ids.0}"
   trusted_user_ca        = "${var.trusted_user_ca}"
   vpc_security_group_ids = ["${data.terraform_remote_state.vault.bastion_security_group}"]
