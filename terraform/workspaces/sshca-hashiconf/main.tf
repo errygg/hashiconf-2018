141004f2a224316a2662bee61bf67eb6dc843378
Erik R. Rygg <errygg@gmail.com>
2018-10-11 19:18:53 -0600

    Update module path

--- terraform/workspaces/sshca-hashiconf/main.tf
+++ terraform/workspaces/sshca-hashiconf/main.tf
@@ -1,5 +1,5 @@
 module "ssh_client_ca" {
-  source          = "../modules/terraform-aws-ssh-client-ca"
+  source          = "../../modules/terraform-aws-ssh-client-ca"
   consul_version  = "${var.consul_version}"
   consul_url      = "${var.consul_url}"
   trusted_user_ca = "${var.trusted_user_ca}"
