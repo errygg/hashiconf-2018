110fbe43f6c150226b5e9489bc56ab40a4a7938f
Erik R. Rygg <errygg@gmail.com>
2018-10-11 19:12:40 -0600

    Fix module location

--- terraform/workspaces/sshotp-hashiconf/main.tf
+++ terraform/workspaces/sshotp-hashiconf/main.tf
@@ -1,5 +1,5 @@
 module "ssh_client_otp" {
-  source         = "../../modules/terraform-aws-ssh-client-otp"
+  source         = "../modules/terraform-aws-ssh-client-otp"
   consul_version = "${var.consul_version}"
   consul_url     = "${var.consul_url}"
 }
