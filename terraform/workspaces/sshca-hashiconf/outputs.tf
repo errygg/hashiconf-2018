7d02baa76fc4f224a4d16a91c083e1e4b001d79f
Erik R. Rygg <errygg@gmail.com>
2018-10-15 20:17:31 -0600

    Fix CA reference

--- terraform/workspaces/sshca-hashiconf/outputs.tf
+++ terraform/workspaces/sshca-hashiconf/outputs.tf
@@ -5,5 +5,5 @@ output "ca_client_public_ip" {
 
 output "ca_client_private_ip" {
   description = "SSH CA Client Private IP Address"
-  value       = "${module.ssh_client_otp.ca_client_private_ip}"
+  value       = "${module.ssh_client_ca.ca_client_private_ip}"
 }
